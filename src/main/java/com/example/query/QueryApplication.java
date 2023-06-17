dd846ac839207548d7fc7689925d49ef544775e8
Denji27 <duyanhaa1@gmail.com>
2023-06-17 14:07:06 +0700

    cache redis

--- src/main/java/com/example/query/QueryApplication.java
+++ src/main/java/com/example/query/QueryApplication.java
@@ -2,24 +2,47 @@ package com.example.query;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+import java.time.Duration;
+import java.util.Arrays;
+
 @SpringBootApplication
 @EnableCaching
-//@EnableRedisRepositories
+@EnableRedisRepositories
 public class QueryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QueryApplication.class, args);
 	}
+	@Bean
+	public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+		return (builder) -> builder
+				.withCacheConfiguration("consumptions",
+						RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)))
+				.withCacheConfiguration("avgConsumption",
+						RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
+	}
+
+
+
 //	@Bean
-//	public CacheManager cacheManager() {
-//		return new ConcurrentMapCacheManager("product");
+//	public CacheManager cacheManager(){
+//		SimpleCacheManager cacheManager = new SimpleCacheManager();
+//		cacheManager.setCaches(Arrays.asList(
+//				new ConcurrentMapCache("consumptions"),
+//				new ConcurrentMapCache("avgConsumption")
+//		));
+//		return cacheManager;
 //	}
 //	@Bean
 //	public JedisConnectionFactory connectionFactory() {
---
+++ src/main/java/com/example/query/config/CacheConfig.java
@@ -0,0 +1,27 @@
+//package com.example.query.config;
+//
+//import org.springframework.cache.CacheManager;
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.cache.concurrent.ConcurrentMapCache;
+//import org.springframework.cache.interceptor.SimpleCacheErrorHandler;
+//import org.springframework.cache.support.SimpleCacheManager;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.lang.reflect.Array;
+//import java.util.Arrays;
+//
+//@Configuration
+//@EnableCaching
+//public class CacheConfig {
+//    @Bean
+//    public CacheManager cacheManager(){
+//        SimpleCacheManager cacheManager = new SimpleCacheManager();
+//        cacheManager.setCaches(Arrays.asList(
+//                new ConcurrentMapCache("consumptions"),
+//                new ConcurrentMapCache("avgConsumption")
+//        ));
+//        return cacheManager;
+//    }
+//
+//}
