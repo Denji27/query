0c86195642a36b301f0224a97e472f2203487e9e
Denji27 <duyanhaa1@gmail.com>
2023-07-14 16:13:22 +0700

    cmt

--- src/main/java/com/example/query/config/CacheConfig.java
+++ src/main/java/com/example/query/config/CacheConfig.java
@@ -1,18 +1,16 @@
 package com.example.query.config;
-
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
-import org.springframework.cache.CacheManager;
+
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.interceptor.SimpleCacheErrorHandler;
-import org.springframework.cache.support.SimpleCacheManager;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 
-import java.lang.reflect.Array;
+
+
 import java.time.Duration;
-import java.util.Arrays;
+
 
 @Configuration
 @EnableCaching
@@ -39,5 +37,4 @@ public class CacheConfig {
                 .withCacheConfiguration("countConsumption",
                         RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
     }
-
 }
